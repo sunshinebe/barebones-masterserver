958d11c73fedfc340926c073b988a4149b909e71
sunshinebe <sunboycg@gmail.com>
2020-05-18 20:56:07 +0800

    调整bug

--- MasterServerFramework2/Assets/MsfWikiExamples/ForTutorial/CustomNetworkManager.cs
+++ MasterServerFramework2/Assets/MsfWikiExamples/ForTutorial/CustomNetworkManager.cs
@@ -12,9 +12,14 @@ public class CustomNetworkManager : NetworkManager
     //    var player = Instantiate(playerPrefab);
 
     //    var customPlayer = playerPrefab.GetComponent<CustomPlayer>();
-    //    customPlayer.UsernameMesh.text = "Random" + (int) (Random.value*100);
+    //    customPlayer.UsernameMesh.text = "Random" + (int)(Random.value * 100);
 
     //    NetworkServer.AddPlayerForConnection(conn, player.gameObject, 0);
 
+    //}
+    //public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
+    //{
+    //    base.OnServerAddPlayer(conn, playerControllerId, extraMessageReader);
+    //    // your code
     //}
 }
